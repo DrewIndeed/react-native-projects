6ec92e4555f3eb4840d56f6efbb9d58abeaf6861
Andrew Le <s3820098@rmit.edu.vn>
2022-03-31 11:42:50 +0700

    10. workout-app: using FlatList to render work out items names

--- workout-app/screens/HomeScreen.tsx
+++ workout-app/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
 import data from '../data.json';
 
@@ -6,7 +6,18 @@ const HomeScreen = ({ navigation }: NativeStackHeaderProps) => {
   return (
     <View style={styles.container}>
       <Text>HomeScreen</Text>
-      <Text>{JSON.stringify(data)}</Text>
+      <FlatList
+        data={data} // data here is the WHOLE ARRAY OF OBJECTS
+        keyExtractor={(item) => item.slug} // key attribiute when you render a list in Reacr
+        renderItem={(
+          { item } // remember to deconstruct "item"
+        ) => (
+          <View>
+            {/* render items name here */}
+            <Text>{item.name}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
