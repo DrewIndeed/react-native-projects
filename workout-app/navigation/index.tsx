68796bae4f7114547516940d22a01e635b927deb
Andrew Le <s3820098@rmit.edu.vn>
2022-03-31 10:59:16 +0700

    6. workout-app: bottom tab navigation

--- workout-app/navigation/index.tsx
+++ workout-app/navigation/index.tsx
@@ -1,17 +1,28 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from '../screens/HomeScreen';
 import PlannerScreen from '../screens/PlannerScreen';
 
-const Stack = createNativeStackNavigator();
-
-export default function NavigationBar() {
+export default function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Planner">
-        <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
-        <Stack.Screen name="Planner" component={PlannerScreen}></Stack.Screen>
-      </Stack.Navigator>
+      <RootNavigator/>
     </NavigationContainer>
   );
 }
+
+const Stack = createNativeStackNavigator();
+const RootNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Root" component={BottomTabNavigator}></Stack.Screen>
+  </Stack.Navigator>
+);
+
+const BottomTab = createBottomTabNavigator();
+const BottomTabNavigator = () => (
+  <BottomTab.Navigator>
+    <BottomTab.Screen name="Home" component={HomeScreen} />
+    <BottomTab.Screen name="Planner" component={PlannerScreen} />
+  </BottomTab.Navigator>
+);
