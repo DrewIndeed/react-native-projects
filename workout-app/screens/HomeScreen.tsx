cdac9a3ae5afa16489344ccf36f6a7effa218cbc
Andrew Le <s3820098@rmit.edu.vn>
2022-03-31 11:58:26 +0700

    11. workout-app: handle types for (application) data

--- workout-app/screens/HomeScreen.tsx
+++ workout-app/screens/HomeScreen.tsx
@@ -1,22 +1,35 @@
 import { View, Text, StyleSheet, FlatList } from 'react-native';
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
 import data from '../data.json';
+import { Workout } from '../types/data';
+
+const renderItem = ({ item }: { item: Workout }) => (
+  <View>
+    <Text>{item.name}</Text>
+  </View>
+);
 
 const HomeScreen = ({ navigation }: NativeStackHeaderProps) => {
   return (
     <View style={styles.container}>
       <Text>HomeScreen</Text>
       <FlatList
-        data={data} // data here is the WHOLE ARRAY OF OBJECTS
+        // data here is the WHOLE ARRAY OF OBJECTS
+        // data is read from a JSON file so dont know what tah hell is the tpye
+        // so we need to specify which type of array it is
+        data={data as Workout[]}
         keyExtractor={(item) => item.slug} // key attribiute when you render a list in Reacr
-        renderItem={(
-          { item } // remember to deconstruct "item"
-        ) => (
-          <View>
-            {/* render items name here */}
-            <Text>{item.name}</Text>
-          </View>
-        )}
+        // WRITE INLINE WAY
+        // renderItem={(
+        //   { item } // remember to deconstruct "item"
+        // ) => (
+        //   <View>
+        //     {/* render items name here */}
+        //     <Text>{item.name}</Text>
+        //   </View>
+        // )}
+
+        renderItem={renderItem}
       />
     </View>
   );
---
+++ workout-app/types/data.ts
@@ -0,0 +1,26 @@
+export type Difficulty = 'easy' | 'normal' | 'hard';
+
+export interface Workout {
+  slug: string;
+  name: string;
+  duration: number;
+  // declare custom type to be more specific
+  difficulty: Difficulty;
+
+  // declare custom interface because it is an array of custom objects
+  // how to read: array of Sequence Item
+  sequence: SequenceItem[];
+}
+
+export type SequenceType = 'exercise' | 'stretch' | 'break';
+export interface SequenceItem {
+  slug: string;
+  name: string;
+
+  // declare custom type to be more specific
+  type: SequenceType;
+  duration: number;
+
+  // '?': because some exercise don't have its
+  reps?: number;
+}
