8d7a3c563ca6ce025cc2f7ea8d7474513f9fca0e
Andrew Le <s3820098@rmit.edu.vn>
2022-03-31 11:12:19 +0700

    7. workout-app: add options to hide header and handle unmount of screens

--- workout-app/navigation/index.tsx
+++ workout-app/navigation/index.tsx
@@ -7,7 +7,7 @@ import PlannerScreen from '../screens/PlannerScreen';
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <RootNavigator/>
+      <RootNavigator />
     </NavigationContainer>
   );
 }
@@ -15,14 +15,29 @@ export default function Navigation() {
 const Stack = createNativeStackNavigator();
 const RootNavigator = () => (
   <Stack.Navigator>
-    <Stack.Screen name="Root" component={BottomTabNavigator}></Stack.Screen>
+    <Stack.Screen
+      name="Root"
+      component={BottomTabNavigator}
+      // headerShown to false to hide to header part
+      options={{ headerShown: false }}
+    />
   </Stack.Navigator>
 );
 
 const BottomTab = createBottomTabNavigator();
 const BottomTabNavigator = () => (
   <BottomTab.Navigator>
-    <BottomTab.Screen name="Home" component={HomeScreen} />
-    <BottomTab.Screen name="Planner" component={PlannerScreen} />
+    <BottomTab.Screen
+      name="Home"
+      component={HomeScreen}
+      // unmountOnBlur to unmount other page in nav when jump to another page
+      // print out msg in useEffect of the screens to see if needed
+      options={{ unmountOnBlur: true }}
+    />
+    <BottomTab.Screen
+      name="Planner"
+      component={PlannerScreen}
+      options={{ unmountOnBlur: true }}
+    />
   </BottomTab.Navigator>
 );
