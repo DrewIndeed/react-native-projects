247a207b171f7e4e4ae462db281298e0c93294b2
Andrew Le <s3820098@rmit.edu.vn>
2022-04-04 10:40:16 +0700

    21. workout-app: handle children type 3 (best way)

--- workout-app/components/styled/MontserratText.tsx
+++ workout-app/components/styled/MontserratText.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-const MontserratText = ({ children }: { children: Text["props"]["children"] }) => {
-  return <Text style={{ fontFamily: 'montserrat' }} children={children} />;
+const MontserratText = (props: Text['props']) => {
+  return (
+    <Text
+      style={{ fontFamily: 'montserrat' }}
+      {...props} // or children={props.children}
+    />
+  );
 };
 
 export default MontserratText;
