d7261a0fd278e745b0ebe151a35dd87d7244972d
Andrew Le <s3820098@rmit.edu.vn>
2022-04-04 10:36:24 +0700

    19. workout-app: handle children type 1

--- workout-app/components/styled/MontserratText.tsx
+++ workout-app/components/styled/MontserratText.tsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import { Text } from 'react-native';
 
-const MontserratText = ({ children }: { children: any }) => {
-  return <Text style={{ fontFamily: 'montserrat' }}>{children}</Text>;
+const MontserratText = ({ children }: { children: React.ReactNode }) => {
+  return <Text style={{ fontFamily: 'montserrat' }} children={children} />;
 };
 
 export default MontserratText;
