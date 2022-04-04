9d7ca09d83c4f0f9c99eeb6525780fb8a1247ad6
Andrew Le <s3820098@rmit.edu.vn>
2022-04-04 14:44:57 +0700

    37. workout-app: simple usage of useIsFocused hook

--- workout-app/hooks/useWorkouts.ts
+++ workout-app/hooks/useWorkouts.ts
@@ -1,17 +1,20 @@
 import { Workout } from '../types/data';
 import { getWorkouts } from '../storage/workout';
 import { useState, useEffect } from 'react';
+import { useIsFocused } from '@react-navigation/native';
 
 export const useWorkouts = () => {
   const [workouts, setWorkouts] = useState<Workout[]>([]); // if not specified, [] in useState will have type of 'never[]'
+  const isFocused = useIsFocused();
 
   useEffect(() => {
     async function getWorkoutDataFromAsyncStorage() {
+      console.log('Getting data ...');
       const _workouts = await getWorkouts();
       await setWorkouts(_workouts);
     }
 
-    getWorkoutDataFromAsyncStorage();
+    if (isFocused) getWorkoutDataFromAsyncStorage();
   }, []);
 
   return workouts;
