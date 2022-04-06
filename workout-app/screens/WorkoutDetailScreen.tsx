f73a72a803c9da4f97eb604a4b22922f88721c09
Andrew Le <s3820098@rmit.edu.vn>
2022-04-06 10:36:21 +0700

    53. workout-app: implement accessing sequence duration and counting down

--- workout-app/screens/WorkoutDetailScreen.tsx
+++ workout-app/screens/WorkoutDetailScreen.tsx
@@ -5,8 +5,9 @@ import CustomModal from '../components/styled/CustomModal';
 import PressableText from '../components/styled/PressableText';
 import { formatSec } from '../utils/time';
 import { FontAwesome } from '@expo/vector-icons';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import WorkoutItem from '../components/WorkoutItem';
+import { SequenceItem } from '../types/data';
 
 type NavigateParams = {
   route: {
@@ -22,7 +23,54 @@ const WorkoutDetailScreen = ({ route }: DetailNavigation) => {
   // if you place this line below useWorkoutBySlug(), there will be an error: the hook is renderred more time than the previous render
   // const [isModalVisible, setModalVisible] = useState(false);
 
+  // use hook to get workout data based on slug
   const workoutBySlug = useWorkoutBySlug(route.params.slug);
+
+  // state to keep track of sequence belonging to that particular workout
+  const [sequence, setSequence] = useState<SequenceItem[]>([]);
+
+  // state to keep track of duration count down of workout sequence ITEM
+  /* use -1 because:
+  + It is safe when it is use to access an array or something because index of array start at 0 and
+  + It indicates null state of counting 
+  */
+  const [currentDuration, setCurrentDuration] = useState(-1);
+
+  // state to keep track of current index of current sequence item
+  const [idxTracker, setIdxTracker] = useState(-1);
+
+  // ! useEffect here right after all init of useState or other init variables
+  useEffect(() => {
+    // if there is no item in the sequence array, do nothing
+    if (idxTracker === -1) return;
+
+    // otherwise, keep track of duration of current item in sequence
+    setCurrentDuration(workoutBySlug!.sequence[idxTracker].duration);
+
+    // use tracking duration to count down
+    const intervalId = window.setInterval(() => {
+      // update by decrementing currentDuration
+      // here, we use the functional form of use state content
+      setCurrentDuration((curDur) => {
+        console.log(curDur);
+        return curDur - 1;
+      });
+    }, 100);
+
+    // IMPORTANT: need to remove it when done using, otherwise, memory leak when it keeps counting down
+    // this is also called Cleaning
+    return () => window.clearInterval(intervalId);
+  }, [idxTracker]); // it will depend on idxTracker
+
+  // method to add item to sequence
+  const addItemToSequence = (idx: number) => {
+    // concat new item to sequence array for counting down
+    setSequence([...sequence, workoutBySlug!.sequence[idx]]);
+
+    // update current idx of current sequence item
+    setIdxTracker(idx);
+  };
+
   if (!workoutBySlug) return null;
 
   return (
@@ -49,6 +97,15 @@ const WorkoutDetailScreen = ({ route }: DetailNavigation) => {
           </View>
         </CustomModal>
       </WorkoutItem>
+      <View>
+        {sequence.length === 0 && (
+          <FontAwesome
+            name="play-circle-o"
+            size={80}
+            onPress={() => addItemToSequence(0)}
+          />
+        )}
+      </View>
     </View>
   );
 };
