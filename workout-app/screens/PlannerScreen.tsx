797be5e4edc42d65c9f0de5cb9ecf8d2aec3914a
Andrew Le <s3820098@rmit.edu.vn>
2022-04-07 09:10:14 +0700

    75. workout-app: create SequenceItem from form data
    - npm i slugify

--- workout-app/screens/PlannerScreen.tsx
+++ workout-app/screens/PlannerScreen.tsx
@@ -1,11 +1,25 @@
 import { View, StyleSheet } from 'react-native';
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
 import WorkoutForm, { WorkoutFormSubmit } from '../components/WorkoutForm';
+import { SequenceItem, SequenceType } from '../types/data';
+import slugify from 'slugify';
 
 const PlannerScreen = ({ navigation }: NativeStackHeaderProps) => {
     // method to handle form submission
     const handleOnSubmit = (form: WorkoutFormSubmit) => {
-        alert(`${form.name} - ${form.duration} - ${form.reps} - ${form.type}`);
+        const sequenceItem: SequenceItem = {
+            slug: slugify(form.name + ' ' + Date.now(), {
+                lower: true,
+                trim: true,
+            }),
+            name: form.name,
+            type: form.type as SequenceType,
+            duration: Number(form.duration),
+        };
+
+        if (!form.reps) sequenceItem.reps = Number(form.reps);
+
+        console.log(sequenceItem);
     };
 
     return (
