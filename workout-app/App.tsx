cd1a6333032c24af55773b732d002a7cc369c875
Andrew Le <s3820098@rmit.edu.vn>
2022-05-03 23:37:25 +0700

    111. testing: test 1D/2D code reader

--- workout-app/App.tsx
+++ workout-app/App.tsx
@@ -1,19 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import Navigation from './navigation/index';
-import useCachedResources from './hooks/useCachedResources';
+import { StyleSheet, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
-  const isLoaded = useCachedResources();
-  console.log('isLoaded:', isLoaded);
+  return (
+    <View style={styles.container}>
+      <WebView
+        allowsInlineMediaPlayback={true}
+        source={{
+          uri: 'https://drewindeed.github.io/test-qr-scan.github.io/',
+        }}
+        startInLoadingState={true}
+        onShouldStartLoadWithRequest={(event) => {
+          if (
+            event.url !== 'https://drewindeed.github.io/test-qr-scan.github.io/'
+          ) {
+            // print out the url when scanned QR code successfully
+            console.log('onShouldStartLoadWithRequest', event.url);
 
-  if (isLoaded) {
-    return (
-      <>
-        <Navigation />
-        <StatusBar style="auto" />
-      </>
-    );
-  } else {
-    return null;
-  }
+            // Code to use the url further goes here.
+
+            return false;
+          }
+          return true;
+        }}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+// import { StatusBar } from 'expo-status-bar';
+// import Navigation from './navigation/index';
+// import useCachedResources from './hooks/useCachedResources';
+
+// export default function App() {
+//   const isLoaded = useCachedResources();
+//   console.log('isLoaded:', isLoaded);
+
+//   if (isLoaded) {
+//     return (
+//       <>
+//         <Navigation />
+//         <StatusBar style="auto" />
+//       </>
+//     );
+//   } else {
+//     return null;
+//   }
+// }
